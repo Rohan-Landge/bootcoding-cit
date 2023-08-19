d0cbb7af73236d809c6d5d3b49ee7c548e737608
Naynesh-coder <naineszade1@gmail.com>
2023-08-19 12:01:34 +0530

    logical opretions

---
+++ src/java/array/assiment1.java
@@ -0,0 +1,17 @@
+package array;
+
+public class assiment1 {
+    public static void main(String[] args) {
+        //declare array
+        int[] temp;
+        //Declare + initialize array
+        int[] arr = {5,15,10,3,8};
+        //Lenght of araay
+        int n = arr.length;
+        //traverse array
+        for(int i=0;i<=4;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
